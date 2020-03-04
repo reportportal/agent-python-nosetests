5a5daf133cec109c55812c7cbfa910095bf5f310
Dzmitry Kolb <c-dzmitry.kolb@hulu.com>
2020-03-04 11:49:19 +0300

    freeze client version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tar_url = 'https://github.com/reportportal/agent-python-nosetests'
 
 
 requirements = [
-    'reportportal-client>=3.1.0',
+    'reportportal-client==3.2.3',
     'nose>=1.3.0',
 ]
 
